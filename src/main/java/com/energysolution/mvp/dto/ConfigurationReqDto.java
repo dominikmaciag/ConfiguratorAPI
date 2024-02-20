bd7883d6cf7684797f33714a236dc823b8585f40
DESKTOP-JUOF4AN\mail <mail@dominikmaciag.com>
2024-02-20 05:29:34 +0100

    added annotation @NotBlank

--- src/main/java/com/energysolution/mvp/dto/ConfigurationReqDto.java
+++ src/main/java/com/energysolution/mvp/dto/ConfigurationReqDto.java
@@ -1,5 +1,6 @@
 package com.energysolution.mvp.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,10 @@ import lombok.Setter;
 @Setter
 public class ConfigurationReqDto {
 
+    @NotBlank(message = "Identifier is required")
     private String identifier;
+
+    @NotBlank(message = "Configuration is required")
     private String configuration;
 
 }
